4850803bdc1c5b1d765725f49451419b01850a59
Novel Corpse <romandafe94@gmail.com>
2019-03-03 23:07:46 +0300

    refs #0; created libcurl example;

---
+++ c/libcurl/main.c
@@ -0,0 +1,50 @@
+#include <curl/curl.h>
+#include <stdio.h>
+
+void usage() {
+  fprintf(stdout, "usage:\n");
+  fprintf(stdout, "./out <url>\n");
+}
+
+int main(int argc, char** argv) {
+
+  if (argc < 2) {
+    usage();
+    return 0;
+  }
+
+	CURL* handle = curl_easy_init();
+  FILE* fd;
+	CURLcode res;
+  char *location;
+  long response_code;
+	curl_easy_setopt(handle, CURLOPT_URL, argv[1]);
+	res = curl_easy_perform(handle);
+
+	if (res != CURLE_OK)
+		fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+	else {
+      res = curl_easy_getinfo(handle, CURLINFO_RESPONSE_CODE, &response_code);
+      if((res == CURLE_OK) &&
+         ((response_code / 100) != 3)) {
+        /* a redirect implies a 3xx response code */ 
+        fprintf(stderr, "Not a redirect.\n");
+      }
+      else {
+        res = curl_easy_getinfo(handle, CURLINFO_REDIRECT_URL, &location);
+ 
+        if((res == CURLE_OK) && location) {
+        	fd = fopen("./downloaded", "w");
+
+          curl_easy_setopt(handle, CURLOPT_URL, location);
+          curl_easy_setopt(handle, CURLOPT_WRITEDATA, fd);
+          curl_easy_perform(handle);
+        }
+      }
+    }
+
+	curl_easy_cleanup(handle);
+  fclose(fd);
+	return 0;
+}
