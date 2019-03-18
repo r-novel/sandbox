acab9a285ac8a139a772f3b7776ba03149a0549f
Novel Corpse <romandafe94@gmail.com>
2019-03-18 23:17:19 +0300

    refs #2; created simple progress-bar;

---
+++ c/progress-bar/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define block "█";
+#define prefix "[\033[1;35m\t\tProgress:\033[0m [\033[1;32m"
+#define suffix "\033[0m] "
+
+static void move(double in) {
+	for(int i = 0; i < in; ++i)
+		printf("█");
+	printf("%s\033[1;35m%.2f%%\r\033[0m", suffix, in);
+	fflush(stdout);
+	sleep(1);
+}
+
+void Do(double step) {
+	printf("\n");
+	for (int i = 0; i <= step; ++i) {
+		printf("%s", prefix);
+		move((i / step) * 100.);
+	}
+	printf("\n\n");
+}
+int main(int argc, char** argv) {
+	if (argc < 2) {
+		fprintf(stderr, "\033[1;36m Usage: ./out <steps> \n\033[0m");
+		return -1;
+	}
+	Do(strtol(argv[1], NULL, 10));
+
+	return 0;
+}
+
+
