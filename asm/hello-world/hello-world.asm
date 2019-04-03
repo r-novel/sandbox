1303f009fe63c7e035de393e1ff1fcda4c3472ab
Novel Corpse <romandafe94@gmail.com>
2019-04-03 23:10:52 +0300

    refs #4; assembler sandbox part started; added simple hello world program written in nasm;

---
+++ asm/hello-world/hello-world.asm
@@ -0,0 +1,16 @@
+; nasm hello world; 
+
+global _main
+section .text
+_main:
+    mov     rax, 0x2000004
+    mov     rdi, 1
+    mov     rsi, msg
+    mov     rdx, msg.len
+    syscall
+    mov     rax, 0x2000001
+    mov     rdi, 0
+    syscall
+section .data
+msg:    db      "Hello, world!", 10
+.len:   equ     $ - msg
