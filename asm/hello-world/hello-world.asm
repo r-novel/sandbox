5c1060fcdbeee43b174c1a483a874b95dca7f08f
Novel Corpse <romandafe94@gmail.com>
2019-04-03 23:21:25 +0300

    refs #4; move data section to top;

--- asm/hello-world/hello-world.asm
+++ asm/hello-world/hello-world.asm
@@ -1,7 +1,13 @@
-; nasm hello world; 
+; nasm hello world;
 
 global _main
+
+section .data
+		msg: db "Hello, world!", 10
+		.len: equ $-msg
+
 section .text
+
 _main:
     mov     rax, 0x2000004
     mov     rdi, 1
@@ -11,6 +17,3 @@ _main:
     mov     rax, 0x2000001
     mov     rdi, 0
     syscall
-section .data
-msg:    db      "Hello, world!", 10
-.len:   equ     $ - msg
