f585a971ccdc08f11610e2999936c4cc98e08b91
Novel Corpse <romandafe94@gmail.com>
2019-06-27 08:19:28 +0300

    refs #0; portable alignof definition; renamed packing to alignment example;

---
+++ c/alignment/alignof-example.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stddef.h>
+
+#define alignof(type) ({ \
+    struct s { char c; type d; }; \
+    offsetof(struct s, d); \
+})
+
+int main(int argc, char const *argv[]) {
+	printf("align of int is %zd\n", alignof(int))
+	return 0;
+}
