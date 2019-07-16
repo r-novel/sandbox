19aafaec43dea9889b608aadd82630fe3a3a2d4f
Novel Corpse <romandafe94@gmail.com>
2019-07-16 22:17:46 +0300

    refs #0; added named pipe samples;

---
+++ c/named-pipe/reader.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(void) {
+	int fd;
+
+	char* myfifo = "/tmp/tmp_fifo";
+
+	mkfifo(myfifo, 0666);
+
+	char buf_1[80], buf_2[80];
+	while (1) {
+
+		fd = open(myfifo, O_RDONLY);
+		read(fd, buf_1, 80);
+
+		printf("User first: %s\n", buf_1);
+		close(fd);
+
+		fd = open(myfifo, O_WRONLY);
+		fgets(buf_2, 80, stdin);
+		write(fd, buf_2, strlen(buf_2) + 1);
+		close(fd);
+	}
+	return 0;
+}
