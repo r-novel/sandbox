ad228c76e820ca22948167cc03e5804c78e7e7e7
Novel Corpse <romandafe94@gmail.com>
2019-08-17 23:30:20 +0300

    refs #0; flv-parser started;

---
+++ c/flv-parser/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#pragma pack(push, 1)
+typedef struct {
+    uint8_t                         signature[3];
+    uint8_t                         version;
+    uint8_t                         type_flags;
+    uint32_t                        offset;
+} flv_header_t;
+
+typedef struct {
+    uint8_t                         type;
+    uint32_t                        data_size:24;
+    uint32_t                        timestamp;                /* 24 bit of timestamp; 8 bits of extension; */
+    uint32_t                        stream_id:24;
+    void                           *data;
+} flv_tag_t;
+
+typedef struct {
+    uint8_t                         sound_format:4;
+    uint8_t                         sound_rate:2;
+    uint8_t                         sound_size:1;
+    uint8_t                         sound_type:1;
+    uint8_t                        *sound_data;               /* uint8_t sound_data[flv_tag_t->data_size]; */
+} flv_audio_data_t;
+
+typedef struct {
+    uint8_t                         aac_packet_type;
+    void                           *aac_data;
+} flv_audio_aac_packet_t;
+
+
+typedef struct {
+    uint8_t                           frame_type:4;
+    uint8_t                           codec_id:4;
+    void                             *video_data;
+} flv_video_data_t;
+
+typedef struct {
+    uint8_t                           avc_packet_type;
+    int32_t                           composition_time;
+    void                             *data;
+} flv_video_avc_packet_t;
+#pragma pack(pop)
+
+
+
+int main(int argc, char** argv) {
+  return 0;
+}
