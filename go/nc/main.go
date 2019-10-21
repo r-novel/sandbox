29a975e1585ea839f8463443208eb41a2c2a870d
Novel Corpse <romandafe94@gmail.com>
2019-10-21 21:51:15 +0300

    refs #0; options and params;

--- go/nc/main.go
+++ go/nc/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"flag"
 	"io"
 	"log"
 	"net"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -14,7 +16,15 @@ func main() {
 		}
 	}
 
-	conn, e := net.Dial("tcp", "127.0.0.1:8080")
+	typeConn := flag.String("type", "tcp", "connection type")
+	host := flag.String("host", "0.0.0.0", "server hostname")
+	port := flag.Int("port", 8080, "server port")
+
+	flag.Parse()
+
+	addr := *(host) + ":" + strconv.Itoa(*port)
+
+	conn, e := net.Dial(*typeConn, addr)
 	if e != nil {
 		log.Fatal(e)
 	}
