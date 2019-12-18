7b3c189958b0dc22e62bb0a161d3880bf11782c4
Novel Corpse <romandafe94@gmail.com>
2019-12-18 22:43:37 +0300

    refs #0; attribute cleanup - ansi c defer;

---
+++ c/cleanup/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TMP "/tmp/test-tmp.file"
+
+void __free_buf(char **buf)
+{
+  printf("freeing buffer;\n");
+  free(*buf);
+}
+
+void __free_file(FILE **fd)
+{
+	printf("closing file;\n");
+  fclose(*fd);
+
+  printf("remove the file;\n");
+  remove(TMP);
+}
+
+int main(int argc, char **argv)
+{
+  char *buf __attribute__ ((__cleanup__(__free_buf))) = (char*) malloc(40 * sizeof(char));
+  FILE *fp __attribute__ ((__cleanup__(__free_file)));
+
+  fp = fopen(TMP, "w+");
+  if(fp) fprintf(fp, "%s", "ATestlinewithnospacesandother");
+
+  fflush(fp);
+  fseek(fp, 0L, SEEK_SET);
+  fscanf(fp, "%s", buf);
+  printf("%s\n", buf);
+
+  return 0;
+}
